8e200e0f7862cf7c4e5f88553b9022ac9afe8033
Chris Roberts <code@chrisroberts.org>
2015-06-17 15:34:36 -0700

    Implement server type things (except save)

--- lib/miasma/contrib/local/compute.rb
+++ lib/miasma/contrib/local/compute.rb
@@ -25,6 +25,69 @@ module Miasma
             )
             Server.new(
               self,
+              server_info(lxc)
+            ).valid_state
+          end
+        end
+
+        # Save/create the server
+        #
+        # @param server [Miasma::Models::Compute::Server]
+        # @return [Miasma::Models::Compute::Server]
+        def server_save(server)
+
+        end
+
+        # Reload the server data
+        #
+        # @param server [Miasma::Models::Compute::Server]
+        # @return [Miasma::Models::Compute::Server]
+        def server_reload(server)
+          if(server.persisted?)
+            lxc = Lxc.new(
+              server.name,
+              :base_path => File.dirname(server.id)
+            )
+            server.load_data(
+              server_info(lxc)
+            ).valid_state
+            server
+          else
+            server
+          end
+        end
+
+        # Destroy the server
+        #
+        # @param server [Miasma::Models::Compute::Server]
+        # @return [TrueClass, FalseClass]
+        def server_destroy(server)
+          if(server.persisted?)
+            lxc = Lxc.new(
+              server.name,
+              :base_path => File.dirname(server.id)
+            )
+            if(lxc.exists?)
+              if(lxc.running?)
+                lxc.stop
+              end
+              lxc.destroy
+            end
+            true
+          else
+            false
+          end
+        end
+
+        protected
+
+        # Generate model attributes from container
+        #
+        # @param lxc [Lxc]
+        # @return [Smash]
+        def server_info(lxc)
+          if(lxc.exists?)
+            Smash.new(
               :id => lxc.path.to_s,
               :name => lxc.name,
               :state => lxc.state,
@@ -38,22 +101,17 @@ module Miasma
               ] : [],
               :addresses_private => [],
               :status => lxc.state.to_s
-            ).valid_state
+            )
+          else
+            Smash.new(
+              :id => lxc.path.to_s,
+              :name => lxc.name,
+              :state => :terminated,
+              :addresses_public => [],
+              :addresses_private => [],
+              :status => 'Destroyed'
+            )
           end
-        end
-
-        # Reload the server data
-        #
-        # @param server [Miasma::Models::Compute::Server]
-        # @return [Miasma::Models::Compute::Server]
-        def server_save(server)
-
-        end
-
-        def server_reload(obj)
-        end
-
-        def server_destroy(obj)
         end
 
       end
